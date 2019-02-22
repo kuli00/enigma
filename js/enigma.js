0f40c663ebd3a540fbed5802e23e408ea922839f
Maciej Kuliński <kulinskimm@gmail.com>
2019-02-22 19:00:59 +0100

    Basic keyboard functionality

---
+++ js/enigma.js
@@ -0,0 +1,25 @@
+$(document).keydown(function(e){
+    const keyId = e.keyCode;
+    const keyCodesRow = keyCodes.filter(k => k.keyCode === keyId);
+    const keyLetter = keyCodesRow[0]["letter"];
+    const buttonId = "#" + keyLetter;
+    $(buttonId).css("background-color", "#ffeeba");
+    $(buttonId).css("color", "#000000");
+});
+
+$(document).keyup(function(e){
+    const keyId = e.keyCode;
+    const keyCodesRow = keyCodes.filter(k => k.keyCode === keyId);
+    const keyLetter = keyCodesRow[0]["letter"];
+    const buttonId = "#" + keyLetter;
+    const currentMessage = $("#message").val();
+    $("#message").val(currentMessage + keyLetter);
+    $(buttonId).css("background-color", "#5a6268");
+    $(buttonId).css("color", "#ffffff");
+
+
+});
+
+function resetMessage() {
+    $("#message").val("");
+}
